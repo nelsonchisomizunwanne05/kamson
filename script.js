a59dc802f2963e0d6f3327e08ddf6472cf8550e3
nelsonchisomizunwanne05 <nelsonbarth999@gmail.com>
2025-02-04 21:32:10 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes-btn").addEventListener("click", function(){
+    document.getElementById("response").innerHTML = "YAY! I AM SO HAPPY YOU SAID YES! I LOVE YOU SO MUCH!";
+});
+
+document.getElementById("no-btn").addEventListener("click", function(){
+    document.getElementById("response").innerHTML = "Oh NO! I AM SAD YOU SAID NO! Maybe next time";
+});
